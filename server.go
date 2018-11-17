440d550daed5a449f45c62da05322213aab28513
Dennis273 <dennisc695@gmail.com>
2018-11-17 11:05:42 +0800

    Bug fixed

--- server.go
+++ server.go
@@ -9,8 +9,8 @@ import (
 )
 
 type Form struct {
-	Username string
-	Email    string
+	Name  string
+	Email string
 }
 
 func main() {
@@ -28,28 +28,32 @@ func loadPage(filename string) []byte {
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodPost:
-		f := ParseFrom(w, r)
-		renderForm(w, f)
+		fP := ParseFrom(w, r)
+		fmt.Println(*fP)
+		renderForm(w, *fP)
 	default:
 		index := loadPage("static/index.html")
 		fmt.Fprint(w, string(index))
 	}
 }
 
-func ParseFrom(w http.ResponseWriter, r *http.Request) Form {
+func ParseFrom(w http.ResponseWriter, r *http.Request) *Form {
 	if err := r.ParseForm(); err != nil {
 		fmt.Fprintf(w, "Error")
-		return Form{"null", "null"}
+		return &Form{"null", "null"}
 	}
 	name := r.FormValue("name")
 	email := r.FormValue("email")
 	f := Form{name, email}
-	fmt.Println(f)
-	return f
+	fmt.Println(name)
+	return &f
 }
 
 func renderForm(w http.ResponseWriter, f Form) {
 	fmt.Println("render")
-	t, _ := template.ParseFiles("template/form.html")
+	t, err := template.ParseFiles("template/form.html")
+	if err != nil {
+		panic(err)
+	}
 	t.Execute(w, f)
 }
